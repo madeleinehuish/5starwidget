7538969bd6bb4ab558b588911af2a2cedd49a244
Madeleine Huish <madeleinehuish@gmail.com>
2018-11-02 05:31:59 -0700

    few optimizations

--- src/App.js
+++ src/App.js
@@ -3,26 +3,37 @@ import React, { Component } from 'react';
 export default class App extends Component {
 	constructor(){
 		super();
-		let ratinghover = new Array(6).fill(false); ratinghover[0] = null;
-		let ratingclick = new Array(6).fill(false); ratingclick[0] = null;
+		let rating = new Array(6).fill(false); rating[0] = null;
 
 		this.state = {
-			rating: 0,
-			// clickState: 0,
-			// hoverState: 0,
-			initialRating: ratinghover,
-			ratingh: ratinghover,
-			ratingc: ratingclick
+			initialRating: rating,
+			rating: rating,
+			clickState: 0
 		}
 		this.handleHover = this.handleHover.bind(this);
 		this.handleOut = this.handleOut.bind(this);
+		this.handleClick = this.handleClick.bind(this);
 	}
 
+	// componentDidUpdate() {
+	// 	let num = this.state.ratingc;
+	// 	let newArray = [null];
+	// 	for(let i=1; i <= 5; i++) {
+	// 		if (i<=num) {
+	// 			newArray[i] = true;
+	// 		}
+	// 		else {
+	// 			newArray[i] = false;
+	// 		}
+	// 	}
+	// 	this.setState({ ratingh: newArray });
+	// 	this.forceUpdate();
+	// }
 
 	handleHover(event) {
 		console.log('event: ', event.target);
 		let num = event.target.dataset.rating;
-		this.setState({ rating: num+1});
+		// this.setState({ rating: num+1});
 		let newArray = [null];
 		for(let i=1; i <= 5; i++) {
 			if (i<=num) {
@@ -32,30 +43,48 @@ export default class App extends Component {
 				newArray[i] = false;
 			}
 		}
-		this.setState({ ratingh: newArray });
+		this.setState({ rating: newArray });
 		this.forceUpdate();
 	}
 
 	handleOut() {
 		let newArray = this.state.initialRating;
 		console.log('newArray in handleOut: ', newArray);
-		this.setState({ ratingh: newArray });
+		this.setState({ rating: newArray }); //this needs to be modified for click, check click state
 		this.forceUpdate();
+	}
+
+	handleClick(event) {
+		this.setState({ clickState: event.target.dataset.rating })
 	}
 
 	//add a click function
 
 	render () {
 		console.log('this.state.initialRating: ', this.state.initialRating);
+
+		const listItems = this.state.rating.map((item, index) => {
+			if(index !== 0) {
+				return (
+									<li
+										data-rating={index}
+										className={this.state.rating[index]===true ? 'yellow' : 'grey'}
+										onMouseEnter={this.handleHover}
+										onMouseLeave={this.handleOut}
+										onClick={this.handleClick}
+									>*</li>
+				)
+			}
+		})
+
+		console.log('listItems: ', listItems);
+
+
 		return (
 			<div id="container">
 				<div id="starContainer">
 					<ul id="starList">
-						<li data-rating='1' className={this.state.ratingh[1]===true ? 'yellow' : 'grey'} onMouseEnter={this.handleHover} onMouseLeave={this.handleOut}>*</li>
-						<li data-rating='2' className={this.state.ratingh[2]===true ? 'yellow' : 'grey'} onMouseEnter={this.handleHover} onMouseLeave={this.handleOut}>*</li>
-						<li data-rating='3' className={this.state.ratingh[3]===true ? 'yellow' : 'grey'} onMouseEnter={this.handleHover} onMouseLeave={this.handleOut}>*</li>
-						<li data-rating='4' className={this.state.ratingh[4]===true ? 'yellow' : 'grey'} onMouseEnter={this.handleHover} onMouseLeave={this.handleOut}>*</li>
-						<li data-rating='5' className={this.state.ratingh[5]===true ? 'yellow' : 'grey'} onMouseEnter={this.handleHover} onMouseLeave={this.handleOut}>*</li>
+						{listItems}
 					</ul>
 				</div>
 	</div>
